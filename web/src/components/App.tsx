7ae53bf13074e931cf9323d699e74806f01159f3
Kristiyan Petsanov <kristiqn3456@gmail.com>
2024-05-20 20:47:18 +0300

    Switch to server

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { debugData } from "../utils/debugData";
+import { isEnvBrowser } from "../utils/misc";
 import { fetchNui } from "../utils/fetchNui";
 
-// This will set the NUI to visible if we are
-// developing in browser
 debugData([
   {
     action: "setVisible",
@@ -12,52 +11,22 @@ debugData([
   },
 ]);
 
-interface ReturnClientDataCompProps {
-  data: unknown;
-}
+const App = () => {
+  const [isPhone, setIsPhone] = useState(false);
 
-const ReturnClientDataComp: React.FC<ReturnClientDataCompProps> = ({
-  data,
-}) => (
-  <>
-    <h5>Returned Data:</h5>
-    <pre>
-      <code>{JSON.stringify(data, null)}</code>
-    </pre>
-  </>
-);
+  useEffect(() => {
+    document.getElementsByTagName("html")[0].style.visibility = "visible";
+    document.getElementsByTagName("body")[0].style.visibility = "visible";
 
-interface ReturnData {
-  x: number;
-  y: number;
-  z: number;
-}
-
-const App: React.FC = () => {
-  const [clientData, setClientData] = useState<ReturnData | null>(null);
-
-  const handleGetClientData = () => {
-    fetchNui<ReturnData>("getClientData")
-      .then((retData) => {
-        console.log("Got return data from client scripts:");
-        console.dir(retData);
-        setClientData(retData);
-      })
-      .catch((e) => {
-        console.error("Setting mock data due to error", e);
-        setClientData({ x: 500, y: 300, z: 200 });
-      });
-  };
+    if (!isEnvBrowser()) {
+      // TODO: Get config if is phone or just UI
+    }
+  }, []);
 
   return (
-    <div className="nui-wrapper">
-      <div className="popup-thing">
-        <div>
-          <h1>This is the NUI Popup!</h1>
-          <p>Exit with the escape key</p>
-          <button onClick={handleGetClientData}>Get Client Data</button>
-          {clientData && <ReturnClientDataComp data={clientData} />}
-        </div>
+    <div className={!isPhone ? "default-wrapper" : ""}>
+      <div className="app">
+        <div className="app-wrapper"></div>
       </div>
     </div>
   );
